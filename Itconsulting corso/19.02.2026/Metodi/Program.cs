c218fe261db76307180dad6d4b2b85c00aeb11a7
Fabio Foglieni <fabio.foglieni@hotmail.it>
2026-02-19 15:08:36 +0100

    Svolti esercizi su metodi

--- Itconsulting corso/19.02.2026/Metodi/Program.cs
+++ Itconsulting corso/19.02.2026/Metodi/Program.cs
@@ -6,10 +6,39 @@ class Program
     {
         int a = 2, b = 9;
         Console.WriteLine($"Risultato: {Somma(a, b)}");
+        // ESERCIZIO 1
+        StampaSaluto("Lupo Lucio");
+
+        // ESERCIZIO 2
+        int numero = 11;
+        Console.WriteLine($"Il numero {numero} è pari? {VerificaPari(numero)}");
+
+        // ESERCIZIO 3
+        int baseNum = 15, esponente = 5, risultato = CalcolaPotenza(baseNum, esponente);
+
+        Console.WriteLine($"Il numero {baseNum} elevato alla potenza di {esponente} da come risultato: {risultato}");
     }
 
     private static int Somma(int a, int b)
     {
         return a + b;
+    }
+
+    private static void StampaSaluto(string nome)
+    {
+        Console.WriteLine($"Ciao bambini, sono {nome}!");
+    }
+
+    private static bool VerificaPari(int numero)
+    {
+        return numero % 2 == 0;
+    }
+
+    private static int CalcolaPotenza(int baseNum, int esponente)
+    {
+        int risultato = baseNum;
+        for (int i = 0; i < esponente-1; i++)
+            risultato *= baseNum;
+        return risultato;
     }
 }
