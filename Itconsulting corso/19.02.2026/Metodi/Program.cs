60b19cb6663ae588ccec091387346b95b816ab0e
Fabio Foglieni <fabio.foglieni@hotmail.it>
2026-02-19 14:12:40 +0100

    introduzione metodi

---
+++ Itconsulting corso/19.02.2026/Metodi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 2, b = 9;
+        Console.WriteLine($"Risultato: {Somma(a, b)}");
+    }
+
+    private static int Somma(int a, int b)
+    {
+        return a + b;
+    }
+}
