7ffae46d5eeccd1e20ced12a90d8829cfd480f6d
Fabio Foglieni <fabio.foglieni@hotmail.it>
2026-02-26 14:19:16 +0100

    Enum spiegazione

---
+++ Itconsulting corso/26.02.2026/Enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+enum Prodotto
+{
+    Gelato,
+    Olio,
+    Mela
+}
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Prodotto 1: {Prodotto.Mela}");
+
+        Prodotto p = Prodotto.Gelato;
+
+        switch(p)
+        {
+            case Prodotto.Gelato:
+                Console.WriteLine($"La variabile in analisi è {p}");
+                break;
+            case Prodotto.Olio:
+                Console.WriteLine($"La variabile è {p}");
+                break;
+            case Prodotto.Mela:
+                Console.WriteLine($"La variabile è {p}");
+                break;
+        }
+    }
+}
